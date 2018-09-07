b40aa4c71d16806bb9c31c164573e2568d6778b6
yaser-alazem <yaser.azem@hotmail.com>
2018-09-07 15:02:00 +0300

    second commit

--- src/context.js
+++ src/context.js
@@ -3,15 +3,17 @@ import axios from "axios";
 
 const Context = React.createContext();
 const reducer = (state, action) => {
-  switch(action.type) {
-    case 'SEARCH_TRACKS':
+  switch (action.type) {
+    case "SEARCH_TRACKS":
       return {
         ...state,
         track_list: action.payload,
-        heading: 'Search Results'
-      }
+        heading: "Search Results"
+      };
+    default:
+      return state;
   }
-}
+};
 export class Provider extends Component {
   state = {
     track_list: [],
