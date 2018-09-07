d35a3c5f378799e425b1616f5c600dc2e815a981
yaser-alazem <yaser.azem@hotmail.com>
2018-09-07 16:08:30 +0300

    Replace BrowseRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Provider } from "./context";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/layouts/Navbar";
 import Index from "./components/layouts/Index";
